98536a5e217b401b55bb09e1c9717f1b417cb1aa
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-30 02:25:19 +0100

    Split component

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import dynamic from "next/dynamic";
 const DynamicSidebar = dynamic(() => import("./Sidebar"), {
   ssr: false,
   loading: () => <p>Loading...</p>,
 });
 
-const Layout = ({ children }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="h-screen flex flex-row justify-start">
       <DynamicSidebar />
-      <div className="bg-primary flex-1 p-4 text-white">{children}</div>
+      <div className="bg-primary flex-1  text-white">{children}</div>
     </div>
   );
 };
